59a2b432ba6517de95b695eb9890f56c6f087cf4
CarsonCuster <carsonwcuster@gmail.com>
2020-02-18 17:37:00 -0500

    Added Spawner and SpawnPoint Scripts

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] obstaclePattern;
+
+    public float timeBtwSpawn;
+    public float startTimeBtwSpawn;
+
+    public void Update()
+    {
+        if(timeBtwSpawn <= 0)
+        {
+            int rand = Random.Range(0, obstaclePattern.Length);
+            Instantiate(obstaclePattern[rand], transform.position, Quaternion.identity);
+            timeBtwSpawn = startTimeBtwSpawn;
+
+        }
+        else
+        {
+            timeBtwSpawn -= Time.deltaTime;
+        }
+        
+        
+    }
+
+}
---
+++ Assets/Scripts/Spawnpoint.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnpoint : MonoBehaviour
+{
+    public GameObject item;
+    // Start is called before the first frame update
+     void Start()
+    {
+        Instantiate(item, transform.position, Quaternion.identity);
+    }
+}
