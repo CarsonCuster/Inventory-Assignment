333c7ca755621b11d26e9eb16fd9fa4ee11f1d82
Way Robertson <mizepie@hotmail.com>
2020-02-18 17:01:53 -0500

    Way: Base 'Item' Script
    This commit contains the base "Item" script.
    Still working on the child scripts for this.

---
+++ Assets/Scripts/Items.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// this script handles the base item behaviour
+public class Items : MonoBehaviour
+{
+    public int powerup;
+//will disable itself when it collides with the player
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.CompareTag("Player"))
+        {
+          Debug.Log("you picked me up");
+          //Destroy(gameObject); 
+          gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+        }
+    }
+}
