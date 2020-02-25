310161b199f8ce72b88a9d158790c889665aea56
CarsonCuster <carsonwcuster@gmail.com>
2020-02-25 13:44:22 -0500

    Removed Second PlusPointsItem script

--- Assets/Scripts/PlusPointsItem.cs
+++ Assets/Scripts/PlusPointsItem.cs
@@ -7,7 +7,7 @@ public class PlusPointsItem: Item
     //Description: If the player collides with this item add points to the score  
     override public void HitPlayer(PlayerController pc)
    {
-        pc.score++;//adds a point to the score when the player collideds with this item.
+        pc.score++;//adds a point to the score when the player collides with this item.
         //Destroy(gameObject);// destroys the attached game object.
     }
 }
