40a1f21e4e778422810b4c45d42cd7affffcc480
CarsonCuster <carsonwcuster@gmail.com>
2020-02-27 15:13:02 -0500

    Commented KillPlayer script

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 using Carson;
 public class Item : MonoBehaviour
 {
-  // Base script for the items. Holds the universal behavior all  
+  // Base script for the items. Holds the universal behavior  
     public GameObject item;
   virtual public void HitPlayer(PlayerController pc)// pc = PlayerController.
   {
       Debug.Log("bye");
-      Destroy(gameObject);//Detroys the Item children scripts when they collide with the player.
+      Destroy(gameObject);//Destroys the Item children scripts when they collide with the player.
   }
   
 }
--- Assets/Scripts/KillPlayer.cs
+++ Assets/Scripts/KillPlayer.cs
@@ -6,7 +6,7 @@ public class KillPlayer : Item
 {
     override public void HitPlayer(PlayerController pc) // Pc = PlayerController. 
     {
-        pc.gameObject.SetActive(false);
+        pc.gameObject.SetActive(false); //When this item collides with the player, the player is set to false
         base.HitPlayer(pc);
                 //GetComponent<SpriteRenderer>().color = Color.red;
     }
